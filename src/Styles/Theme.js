eac52fa0a037d84000ec4fb7fe692ded28db392e
myeongjin1115 <myeongjin1115@gmail.com>
2020-02-04 16:11:40 +0900

    #5.2 Footer and Auth UI part Three

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -4,15 +4,15 @@ const BORDER_RADIUS = "4px";
 export default {
   bgColor: "#FAFAFA",
   blackColor: "#262626",
-  darkGrayColor: "#999",
-  lightGrayColor: "#c7c7c7",
-  redColor: "#ed4956",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  redColor: "#ED4956",
   blueColor: "#3897f0",
   darkBlueColor: "#003569",
   boxBorder: "1px solid #e6e6e6",
   borderRadius: "4px",
-  whiteBox: `border:${BORDER_RADIUS};
-             border-radius:${BOX_BORDER};
-            background-color:white;
+  whiteBox: `border:${BOX_BORDER};
+             border-radius:${BORDER_RADIUS};
+             background-color:white;
             `
 };
